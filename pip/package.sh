f8aa57471dbf3298db2bdf53fe9d714632a67b07
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-09-13 14:57:05 +0200

    support for py2 and py3 packages

--- pip/package.sh
+++ pip/package.sh
@@ -5,9 +5,18 @@ if [ -e "${dir}/$1.spec" ] ; then
 else
   echo "### RPM external $1 $2"
   echo "## IMPORT build-with-pip"
+  if [ "$3" = "py2" ] ; then
+    echo "%define doPython3 no"
+  elif [ "$3" == "py3" ] ; then
+    echo "%define doPython2 no"
+  fi
   if [ -f "${dir}/$1.file" ] ; then
     subdir=$(basename $dir)
     echo ""
     echo "## INCLUDE ${subdir}/${1}"
+  fi
+  pycommon=$(echo $1 | sed 's|^py[2-9]-||')
+  if [ -f "${dir}/${pycommon}.file" ] ; then
+    echo "## INCLUDE ${subdir}/${pycommon}"
   fi
 fi
