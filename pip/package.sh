8935edfefd044b1d62724072444bb513565e56a8
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-07-22 16:37:15 +0200

    python 3.9.6 and py2 cleanup

--- pip/package.sh
+++ pip/package.sh
@@ -5,11 +5,6 @@ if [ -e "${dir}/$1.spec" ] ; then
 else
   echo "### RPM external $1 $2"
   echo "## IMPORT build-with-pip"
-  if [ "$3" = "py2" ] ; then
-    echo "%define doPython3 no"
-  elif [ "$3" == "py3" ] ; then
-    echo "%define doPython2 no"
-  fi
   subdir=$(basename $dir)
   pycommon=$(echo $1 | sed 's|^py[2-9]-||')
   if [ -f "${dir}/${pycommon}.file" ] ; then
