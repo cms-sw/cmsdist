c388cf42e86bce8485a0fab09dc13dee97b13d8d
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-09-10 20:08:36 +0200

    cleanup data package generation

--- data/package.sh
+++ data/package.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 dir=$(dirname $0)
-if [ -e "${dir}/$1.spec" ] ; then
-  cat "${dir}/$1.spec"
-  exit 0
-fi
-
 echo "### RPM cms $1 $2"
 if [ -f "${dir}/$1.file" ] ; then
-  cat "${dir}/$1.file"
+  echo "## INCLUDE data/$1"
 else
   case $3 in
     data-build-github)
