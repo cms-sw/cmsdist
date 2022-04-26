dc214304dd69c39307ef226064a0dbeb7aae4fb4
David <David.Lange@cern.ch>
2022-04-26 17:25:32 +0200

    pick up core base envvar

--- scram-tools.file/tools/dd4hep/env.sh
+++ scram-tools.file/tools/dd4hep/env.sh
@@ -1,5 +1,5 @@
 export GCC_GLIBCXX_VERSION=$(gcc -dumpversion | tr '.' '0')
 export CXXMODULE_DATA=""
 if [ "${ROOT_CXXMODULES}" = "1" ] ; then
-  export CXXMODULE_DATA='<flags ROOTCLING_ARGS="-moduleMapFile=$(DD4HEP_BASE)/include/module.modulemap"/>'
+  export CXXMODULE_DATA='<flags ROOTCLING_ARGS="-moduleMapFile=$(DD4HEP_CORE_BASE)/include/module.modulemap"/>'
 fi
